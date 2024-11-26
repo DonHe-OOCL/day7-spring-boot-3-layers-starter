0a01fad420985eb9906f37f49a0b88bf87b1e832
HEDO <hedo@oocl.com>
2024-11-26 11:46:49 +0800

    test: test should_throw_EmployeeAgeInvalidException_when_create_given_a_employee_with_age_6

---
+++ src/main/java/com/oocl/springbootemployee/exception/EmployeeAgeInvalidException.java
@@ -0,0 +1,5 @@
+package com.oocl.springbootemployee.exception;
+
+public class EmployeeAgeInvalidException extends RuntimeException{
+
+}
--- src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
@@ -1,25 +1,36 @@
 package com.oocl.springbootemployee.service;
 
+import com.oocl.springbootemployee.exception.EmployeeAgeInvalidException;
 import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.model.Gender;
 import com.oocl.springbootemployee.repository.EmployeeRepository;
 import com.oocl.springbootemployee.repository.IEmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
+
+    @InjectMocks
+    EmployeeService employeeService;
+
+    @Mock
+    IEmployeeRepository mockedEmployeeRepository;
+
     @Test
     void should_return_the_given_employees_when_getAllEmployees() {
         //given
-        IEmployeeRepository mockedEmployeeRepository = mock(IEmployeeRepository.class);
         when(mockedEmployeeRepository.getAll()).thenReturn(List.of(new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0)));
-        EmployeeService employeeService = new EmployeeService(mockedEmployeeRepository);
 
         //when
         List<Employee> allEmployees = employeeService.getAllEmployees();
@@ -32,15 +43,23 @@ class EmployeeServiceTest {
     @Test
     void should_return_the_created_employee_when_create_given_a_employee() {
         //given
-        IEmployeeRepository mockedEmployeeRepository = mock(IEmployeeRepository.class);
         Employee lucy = new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0);
         when(mockedEmployeeRepository.addEmployee(any())).thenReturn(lucy);
-        EmployeeService employeeService = new EmployeeService(mockedEmployeeRepository);
 
         //when
         Employee createdEmployee = employeeService.creat(lucy);
 
         //then
         assertEquals("Lucy", createdEmployee.getName());
+    }
+
+    @Test
+    public void should_throw_EmployeeAgeInvalidException_when_create_given_a_employee_with_age_6() {
+        // Given
+        Employee kiKi = new Employee(1, "KiKi", 6, Gender.FEMALE, 8000.0);
+        // When
+        // Then
+        assertThrows(EmployeeAgeInvalidException.class, () -> employeeService.creat(kiKi));
+        verify(mockedEmployeeRepository, never()).addEmployee(any());
     }
 }
